38e0b55187a54992efa4e82ab4cabe26ba83cf79
xcoder <onlyforwith@gmail.com>
2026-02-10 20:14:09 +0300

    Install and connect Element Plus UI

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import './style.css';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+
+createApp(App).use(ElementPlus).mount('#app');
