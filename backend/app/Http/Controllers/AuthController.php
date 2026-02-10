06d2abb044bf84aa4dc4bdf9d3ff7ea186ea2310
xcoder <onlyforwith@gmail.com>
2026-02-10 20:54:27 +0300

    Update AuthController with login method

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            abort(401);
+        }
+
+        $token = $request->user()->createToken('web')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'user' => $request->user()
+        ]);
+    }
 }
