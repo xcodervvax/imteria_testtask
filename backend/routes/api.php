e32e9ba9d594a611fb3edd6e919cb66026623587
xcoder <onlyforwith@gmail.com>
2026-02-12 00:50:11 +0300

    Add saveYandex in api route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AccountController;
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
@@ -8,4 +9,12 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post(
+        '/account/yandex',
+        [AccountController::class, 'saveYandex']
+    );
+
 });
