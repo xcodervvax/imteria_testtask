2d46c3ffcdeca3e28b1c0c2b9ff368900c7cd27c
xcoder <onlyforwith@gmail.com>
2026-02-12 00:45:31 +0300

    Fill migration add_yandex_fields_to_user_table

--- backend/database/migrations/2026_02_11_214302_add_yandex_fields_to_users_table.php
+++ backend/database/migrations/2026_02_11_214302_add_yandex_fields_to_users_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('yandex_url')->nullable();
+            $table->string('yandex_org_id')->nullable();
         });
     }
 
@@ -22,7 +23,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn(['yandex_url', 'yandex_org_id']);
         });
     }
 };
