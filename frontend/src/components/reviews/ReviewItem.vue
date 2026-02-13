23bf9d1f7f5254ac93a145dcc6c3672adfbb27ef
xcoder <onlyforwith@gmail.com>
2026-02-14 00:06:22 +0300

    Add ReviewItem component

---
+++ frontend/src/components/reviews/ReviewItem.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue';
+const StarRating = defineAsyncComponent(() => import('../rating/StarRating'));
+
+defineProps<{
+  author: string;
+  rating: number;
+  text: string;
+  reviewDate: string;
+}>();
+</script>
+
+<template>
+  <div class="review-item">
+    <div class="review-header">
+      <div class="author">{{ author }}</div>
+      <StarRating :rating="rating" />
+    </div>
+
+    <div class="date">
+      {{ new Date(reviewDate).toLocaleDateString() }}
+    </div>
+
+    <div class="text">
+      {{ text }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import '../assets/styles/review-item.css';
+</style>
