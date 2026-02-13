8d2862299ee44178d1b6dc5daaf4a3898fee8096
xcoder <onlyforwith@gmail.com>
2026-02-14 00:12:38 +0300

    Add ReviewList component

---
+++ frontend/src/components/reviews/ReviewList.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue';
+import type { Review } from "@/types/review";
+const ReviewItem = defineAsyncComponent(() => import('./ReviewItem.vue'));
+
+defineProps<{
+  reviews: Review[]
+}>();
+</script>
+
+<template>
+  <div>
+    <ReviewItem
+        v-for="review in reviews"
+        :key="review.id"
+        :author="review.author"
+        :rating="review.rating"
+        :text="review.text"
+        :review-date="review.review_date"
+    />
+  </div>
+</template>
---
+++ frontend/src/types/review.ts
@@ -0,0 +1,7 @@
+export interface Review {
+    id: number;
+    author: string;
+    rating: number;
+    text: string;
+    review_date: string;
+}
