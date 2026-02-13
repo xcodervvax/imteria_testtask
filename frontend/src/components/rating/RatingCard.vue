6401d868fde82f1f2b02a4d40d91a9f58bf78cc6
xcoder <onlyforwith@gmail.com>
2026-02-13 23:49:47 +0300

    Add RatingCard component

---
+++ frontend/src/components/rating/RatingCard.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue';
+const StarRating = defineAsyncComponent(() => import('./StarRating.vue'));
+
+defineProps<{
+  rating: number;
+  reviewsCount: number;
+}>();
+</script>
+
+<template>
+  <div class="rating-card">
+    <div class="rating-number">
+      {{ rating.toFixed(1) }}
+    </div>
+
+    <StarRating :rating="rating" />
+
+    <div class="reviews-count">
+      Всего отзывов {{ reviewsCount }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import '../assets/styles/rating-card.css';
+</style>
